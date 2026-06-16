a01e8e2930e3cb94089ad7c1aa1b5e8946b3a8f1
GitLab Runner <gitlab-runner@mtapi.online>
2026-06-16 07:48:09 -0400

    Update Simpified CheckWindowsAccountReadinessAsync by account leverage

---
+++ package/Helpers/ApiExceptionMT4.cs
@@ -0,0 +1,40 @@
+﻿using mt4_term_api;
+using Mt4TermApi;
+using System;
+
+namespace mt4_term_api
+{
+
+    public class ApiExceptionMT4 : Exception
+    {
+        public Error Error { get; }
+
+        public string? ErrorCode => Error?.ErrorCode;
+        public string? ErrorMessageText => Error?.ErrorMessage;
+        public ErrorType ErrorType => Error?.Type ?? default;
+        public MqlErrorCode MqlErrorCode => Error?.MqlErrorCode ?? default;
+        public int MqlErrorIntCode => Error?.MqlErrorIntCode ?? 0;
+        public string? MqlErrorDescription => Error?.MqlErrorDescription;
+        //public MqlErrorTradeCode MqlErrorTradeCode => Error?.MqlErrorTradeCode ?? default;
+        //public int MqlErrorTradeIntCode => Error?.MqlErrorTradeIntCode ?? 0;
+        //public string? MqlErrorTradeDescription => Error?.MqlErrorTradeDescription;
+        public string? RemoteStackTrace => Error?.StackTrace;
+        public string? CommandTypeName => Error?.CommandTypeName;
+        public long CommandId => Error?.CommandId ?? 0;
+
+        public ApiExceptionMT4(Error error)
+            : base(error?.ErrorMessage)
+        {
+            Error = error ?? throw new ArgumentNullException(nameof(error));
+        }
+
+        public override string ToString()
+        {
+            return $"API Exception: {ErrorCode} - {ErrorMessageText}\n" +
+                   $"MQL: {MqlErrorCode} ({MqlErrorIntCode}) - {MqlErrorDescription}\n" +
+                   //$"Trade: {MqlErrorTradeCode} ({MqlErrorTradeIntCode}) - {MqlErrorTradeDescription}\n" +
+                   $"Command: {CommandTypeName} (ID: {CommandId})\n" +
+                   $"Stack: {RemoteStackTrace}";
+        }
+    }
+}
---
+++ package/Helpers/ConnectExceptionMT4.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace mt4_term_api
+{
+    /// <summary>
+    /// Exception thrown when there is a connection issue with the MT4 terminal.
+    /// </summary>
+    /// <remarks>
+    /// This exception is used to indicate that the MT4 terminal could not be connected to.
+    /// </remarks>
+    [Serializable]
+    public class ConnectExceptionMT4 : Exception
+    {
+        public ConnectExceptionMT4()
+        {
+        }
+
+        public ConnectExceptionMT4(string message) : base(message)
+        {
+        }
+
+        public ConnectExceptionMT4(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
